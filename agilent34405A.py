ae5d5a42698109bd83d94dba9cce7a02d8a198d7
Immanuel Amirtharaj <li.chelsey@gmail.com>
2018-01-01 07:02:49 +0000

    agilent34405A.py

---
+++ agilent34405A.py
@@ -0,0 +1,29 @@
+import visa
+
+def agilent34405A():
+    rm = visa.ResourceManager('@py')
+    resources = rm.list_resources()
+    print "Resources:%s"%resources
+    inst = rm.open_resource('USB0::2391::1560::TW47510072::0::INSTR')
+    inst.write('*RST')
+    inst.write('*IDN?')
+    #machine = inst.read()
+    #print machine
+    #inst.write(':INITiate:IMMediate')
+    #inst.write(':FORMat:DATA ASCii')
+    inst.write(':SENSe:RES:RANGe:AUTO')
+    inst.write(':SENSe:RES:RES:AUTO')
+    #inst.write(':MEASure:RES?')
+    inst.write(':MEASure:RESistance?')
+    #inst.write("DATA:DATA? NVMEM")
+    
+    #inst.write(':DATA:POINts? ')
+    #result = inst.query(":FETCh?")
+    #inst.write(":READ?")
+    #reasult = inst.read()
+    result = inst.read()
+    print "Resistance:%s"%result 
+    inst.close()
+
+
+agilent34405A()
